6d560b1000bd764c61472b10e295e13f1c44361d
Wayjourney <email4open@gmail.com>
2024-05-03 18:09:21 +0800

    add alpine.js

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php
 
 function boilerplate_load_assets() {
-  wp_enqueue_script('ourmainjs', get_theme_file_uri('/build/index.js'), array('wp-element'), '1.0', true);
+  // wp_enqueue_script('ourmainjs', get_theme_file_uri('/build/index.js'), array('wp-element'), '1.0', true);
   wp_enqueue_style('ourmaincss', get_theme_file_uri('/build/index.css'));
 }
+
+function enqueue_alpine_js() {
+  wp_enqueue_script('alpine-js', 'https://cdn.jsdelivr.net/npm/alpinejs@3.13.10/dist/cdn.min.js', array(), null, true);
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_alpine_js');
 
 add_action('wp_enqueue_scripts', 'boilerplate_load_assets');
 
