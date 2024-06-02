498d73ab0308894af59f968617e7070d6abf40ef
Wayjourney <email4open@gmail.com>
2024-06-02 23:30:34 +0800

    add about page

---
+++ page-about.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Template Name: About Page
+ *
+ * This is the template that displays the About page.
+ *
+ * @package Ruisitenai
+ */
+
+get_header();
+?>
+About
+<main>
+  <div class="relative overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
+    <div
+      class="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-red-600/10 ring-1 ring-red-50 sm:-mr-80 lg:-mr-96"
+      aria-hidden="true"
+    />
+    <div class="mx-auto max-w-screen-xl px-6 py-32 sm:py-40 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+        <h1 class="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
+        郑州瑞斯特耐有限公司
+        </h1>
+        <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+          <p class="text-lg leading-8 text-gray-600">
+          面对时代的机遇与挑战，始终贯彻“质量为本、专注求真、追求卓 越”的经营理念，以严谨求实的态度和公平诚信的服务思想发展自己，创造价值，回报社会。
+          </p>
+        </div>
+        <img
+          src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80"
+          alt=""
+          class="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
+        />
+      </div>
+    </div>
+    <div class="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
+  </div>
+
+  <div class="mt-32 overflow-hidden sm:mt-40">
+    <div class="mx-auto max-w-screen-xl px-6 lg:flex lg:px-8">
+      <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
+        <div class="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
+          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">公司位置</h2>
+          <p class="mt-6 text-xl leading-8 text-gray-600">
+          郑州瑞斯特耐有限公司总部位于中原腹地新密市，北望黄河、西依嵩山、贯通郑汴洛、连接许平南，是华夏文明的重要发祥地之一。交通位置优越，地处郑州大都市区、郑州国家中心城市
+中心地带和郑州航空港区经济圈，郑少、郑尧、商登高速和郑登快速通道等骨干路网畅通便捷
+          </p>
+        </div>
+        <div class="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
+          <div class="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+            <img
+              src="https://images.unsplash.com/photo-1670272502246-768d249768ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80"
+              alt=""
+              class="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+            />
+          </div>
+          <div class="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
+            <div class="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
+              <img
+                src="https://images.unsplash.com/photo-1605656816944-971cd5c1407f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
+                alt=""
+                class="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+              />
+            </div>
+            <div class="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
+              <img
+                src="https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&h=842&q=80"
+                alt=""
+                class="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+              />
+            </div>
+            <div class="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
+              <img
+                src="https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
+                alt=""
+                class="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="mx-auto my-32 max-w-screen-xl px-6 sm:mt-40 lg:px-8 space-y-20">
+    <div>
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        公司历史
+      </h2>
+      <p class="mt-6 text-base leading-7 text-gray-600">
+      公司历经二十余年发展，已经成为集耐材咨询、研发、生产、销售和筑炉施工为一体的现代化企业。与中钢集团洛阳耐火材料研究院、河南科技大学、武汉科技大学等科研机构长期合作，
+根据《耐火材料标准汇编》不断调整产品结构、积极推动技术革新和成果转换。针对不同客户的不同工况条件进行个性化定制，为国内各大焦化、铝镁、碳素、石化、钢铁、铸造、建材等高温 工业提供高温定型、不定型制品耐材。为客户提供强有力的技术支持，保证产品质量和企业信誉。
+      </p>
+    </div>
+    <div>
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        服务客户
+      </h2>
+      <p class="mt-6 text-base leading-7 text-gray-600">
+      我公司产品广泛应用于钢铁、水泥、有色金属、玻璃、石化、陶瓷、焦化等行业。主要生产有陶瓷蛭石保温板、超轻纳米隔热砖、机制高铝隔热砖、莫来石质隔热砖、聚轻球隔热砖、硅质 隔热砖、高强漂珠砖、粘土质隔热保温砖以及配套的不定型浇注料。产品畅销全国各地，并出又至海外多个国家 (德国、意大利、奥地利、俄罗斯、乌兹别克斯坦、印度尼西亚、土耳其、日
+本、韩国等)，得到了海内外客户的一致认可。
+      </p>
+    </div>
+  </div>
+</main>
+
+<?php get_footer();
